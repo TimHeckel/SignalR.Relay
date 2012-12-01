e4140648927dd0de6e2bb705740d8d0d9422b01f
heck0045 <tim@slatebox.com>
2012-12-01 00:36:08 -0600

    Removed MVC; added httphandler

---
+++ SignalR.Relay/Handlers/SignalRHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.SignalR;
+using System.Web;
+
+namespace SignalR.Relay
+{
+    public class SignalRHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+
+            var c = GlobalHost.ConnectionManager.GetHubContext<RelayHub>();
+            c.Clients.All.foo(2);
+
+            context.Response.Write("EXTERNAL CHECK COMPLETE");
+        }
+    }
+}
